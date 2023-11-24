690ad44725a4994a26fdbbf588b41dd3547c1738
AlanShakir <144908590+AlanShakir@users.noreply.github.com>
2023-11-24 02:35:03 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember(){
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    member.style.display = "none";
+    skills.style.display = "block";
+}
